f17f0c86e94dca193c06d9f88cb1261a31a33994
Dave Strus <dave@getfretless.com>
2017-06-07 11:56:49 -0400

    Create a flick object with a name and id

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const app = {
   init(formSelector) {
+    this.max = 0
     document
       .querySelector(formSelector)
-      .addEventListener('submit', this.addFlick)
+      .addEventListener('submit', this.addFlick.bind(this))
   },
 
   addFlick(ev) {
     ev.preventDefault()
-    const flickName = ev.target.flickName.value
-    console.log(flickName)
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
+
+    console.log(flick.name, flick.id)
+    ++ this.max
   },
 }
 
