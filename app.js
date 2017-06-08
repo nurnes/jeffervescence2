c987197c63bc68d4c738dce6063f8b6662f7ea3d
Naomi Urnes <naomiurnes@gmail.com>
2017-06-08 09:50:57 -0400

    Add flicks to an array

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const app = {
       name: f.flickName.value,
     }
 
+    this.flicks.push(flick)
+
     const listItem = this.renderListItem(flick)
     this.list.appendChild(listItem)
-
-    // TODO: Add flick to this.flicks
 
     ++ this.max
     f.reset()
